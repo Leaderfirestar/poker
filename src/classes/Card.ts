ea8880c7b18076215250f1176e89c7d9b3c3fab6
leaderfirestar <EricAWebb2000@yahoo.com>
2024-12-22 21:48:55 -0600

    flipped boolean

--- src/classes/Card.ts
+++ src/classes/Card.ts
@@ -2,11 +2,14 @@ type Suit = "Heart" | "Diamond" | "Club" | "Spade";
 type CardValue = "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
 
 export class Card {
+	/**	Whether or not the card is flipped (for the river) */
+	flipped: boolean;
 	/**	The Card's suit */
 	suit: Suit;
 	/**	The Card's value */
 	value: CardValue;
 	constructor() {
+		this.flipped = false;
 		this.suit = "Club";
 		this.value = "2";
 	}
