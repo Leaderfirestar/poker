bf67f91e1a11121a4efd4018ac6f5fb9da538042
leaderfirestar <EricAWebb2000@yahoo.com>
2024-12-22 21:48:11 -0600

    Initial card properties

--- src/classes/Card.ts
+++ src/classes/Card.ts
@@ -1,3 +1,13 @@
+type Suit = "Heart" | "Diamond" | "Club" | "Spade";
+type CardValue = "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
+
 export class Card {
-	constructor() { }
+	/**	The Card's suit */
+	suit: Suit;
+	/**	The Card's value */
+	value: CardValue;
+	constructor() {
+		this.suit = "Club";
+		this.value = "2";
+	}
 }
