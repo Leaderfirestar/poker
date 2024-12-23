7feaf73524497226b10b6396f21ff3b45a717063
leaderfirestar <EricAWebb2000@yahoo.com>
2024-12-22 21:41:17 -0600

    initial properties

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -1,5 +1,24 @@
-class Player {
-	constructor() { }
-}
+import { v4 as uuid } from "uuid";
+import { Card } from "./Card";
 
-export default Player;
+export class Player {
+	private id: string = uuid();
+	/** Chip denomination to the number of chips of that denomination */
+	private chips: Map<number, number>;
+	/**	Whether or not this player is the dealer */
+	private dealer: boolean;
+	/**	Whether or not this player has folded */
+	private folded: boolean;
+	/**	The array of cards the player has */
+	private hand: Card[];
+	/**	Player's username */
+	private username: string;
+
+	constructor() {
+		this.chips = new Map();
+		this.folded = false;
+		this.hand = [];
+		this.username = "";
+		this.dealer = false;
+	}
+}
