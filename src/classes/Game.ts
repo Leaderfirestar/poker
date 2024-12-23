03467b602ccbda0f6523693da6c28821ffe52542
leaderfirestar <EricAWebb2000@yahoo.com>
2024-12-22 21:54:44 -0600

    initial properties for the game

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -1,3 +1,23 @@
+import { Card } from "./Card";
+import { Deck } from "./Deck";
+import { Player } from "./Player";
+
 export class Game {
-	constructor() { }
+	/**	Index for the players array to designate a dealer */
+	dealerIndex: number;
+	/**	The deck we'll be using for the game */
+	deck: Deck;
+	/**	The players playing the game */
+	players: Player[];
+	/**	Value of chips in the pot */
+	pot: number;
+	/**	The cards in the river */
+	river: Card[];
+	constructor() {
+		this.dealerIndex = 0;
+		this.deck = new Deck();
+		this.players = [];
+		this.pot = 0;
+		this.river = [];
+	}
 }
