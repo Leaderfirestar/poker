fa97b6d209e33fb3f56cb4dc4ac4302b4bf4a32e
leaderfirestar <EricAWebb2000@yahoo.com>
2024-12-22 22:03:19 -0600

    stage

--- src/classes/Round.ts
+++ src/classes/Round.ts
@@ -1,12 +1,17 @@
 import { Pot } from "./Pot";
 
+type Stage = "pre-flop" | "flop" | "turn" | "river" | "showdown";
+
 export class Round {
 	/**	The current bet for the round */
 	private currentBet: number;
 	/**	Value of chips in the pot */
 	private pot: Pot;
+	/**	The current stage of the round */
+	private stage: Stage;
 	constructor() {
 		this.currentBet = 0;
 		this.pot = new Pot();
+		this.stage = "pre-flop";
 	}
 }
