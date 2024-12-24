f98361220b78c1e24470f98d468440045793c06d
leaderfirestar <EricAWebb2000@yahoo.com>
2024-12-23 20:56:41 -0600

    Created logical deck with cards

--- src/classes/Card.ts
+++ src/classes/Card.ts
@@ -3,14 +3,21 @@ type CardValue = "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q
 
 export class Card {
 	/**	Whether or not the card is flipped (for the river) */
-	private flipped: boolean;
+	private flipped: boolean = false;
 	/**	The Card's suit */
 	private suit: Suit;
 	/**	The Card's value */
 	private value: CardValue;
-	constructor() {
-		this.flipped = false;
-		this.suit = "Club";
-		this.value = "2";
+	constructor(input: CardInput) {
+		this.suit = input.suit;
+		this.value = input.value;
 	}
 }
+
+interface CardInput {
+	suit: Suit;
+	value: CardValue;
+}
+
+export const suits: Suit[] = ["Club", "Diamond", "Heart", "Spade"];
+export const cardValues: CardValue[] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
--- src/classes/Deck.ts
+++ src/classes/Deck.ts
@@ -1,9 +1,37 @@
-import { Card } from "./Card";
+import { Card, cardValues, suits } from "./Card";
 
 export class Deck {
 	/**	The cards in the deck */
 	private cards: Card[];
 	constructor() {
-		this.cards = [];
+		const initialCards: Card[] = [];
+		for (const suit of suits) {
+			for (const value of cardValues) {
+				const card = new Card({ suit, value });
+				initialCards.push(card);
+			}
+		}
+		this.cards = initialCards;
+		this.shuffle();
+	}
+
+	/**
+	 * Shuffles the deck of cards using the Durstenfeld variation of the Fisher-Yates shuffle algorithm.
+	 * 
+	 * This algorithm ensures an unbiased random shuffle by iterating through the array
+	 * from the end to the beginning and swapping each element with a randomly chosen
+	 * element that comes before it (including itself).
+	 * 
+	 * @returns The shuffled array of cards.
+	 * @see {@link https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm Fisher-Yates Shuffle on Wikipedia}
+	 */
+	private shuffle(): Card[] {
+		for (let i = this.cards.length - 1; i >= 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			const temp = this.cards[i];
+			this.cards[i] = this.cards[j];
+			this.cards[j] = temp;
+		}
+		return this.cards;
 	}
 }
