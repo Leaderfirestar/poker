bbc8edc22fe7209fef948681be3cf19a2b5130db
leaderfirestar <EricAWebb2000@yahoo.com>
2024-12-23 18:36:01 -0600

    moved logic from game to round

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -1,20 +1,10 @@
-import { Card } from "./Card";
-import { Deck } from "./Deck";
 import { Player } from "./Player";
 
 export class Game {
-	/**	Index for the players array to designate a dealer */
-	private dealerIndex: number;
-	/**	The deck we'll be using for the game */
-	private deck: Deck;
 	/**	The players playing the game */
 	private players: Player[];
-	/**	The cards in the river */
-	private river: Card[];
+
 	constructor() {
-		this.dealerIndex = 0;
-		this.deck = new Deck();
 		this.players = [];
-		this.river = [];
 	}
 }
--- src/classes/Round.ts
+++ src/classes/Round.ts
@@ -3,15 +3,21 @@ import { Pot } from "./Pot";
 type Stage = "pre-flop" | "flop" | "turn" | "river" | "showdown";
 
 export class Round {
+	/**	The value of the big blind */
+	private bigBlind: number;
 	/**	The current bet for the round */
 	private currentBet: number;
 	/**	Value of chips in the pot */
 	private pot: Pot;
+	/**	The value of the small blind */
+	private smallBlind: number;
 	/**	The current stage of the round */
 	private stage: Stage;
 	constructor() {
+		this.bigBlind = 0;
 		this.currentBet = 0;
 		this.pot = new Pot();
+		this.smallBlind = 0;
 		this.stage = "pre-flop";
 	}
 }
