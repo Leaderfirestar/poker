ad8c558a5676eab23cdd8408b003066922fc8177
leaderfirestar <EricAWebb2000@yahoo.com>
2024-12-23 18:49:08 -0600

    round

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -1,5 +1,6 @@
 import { GameSettings } from "./GameSettings";
 import { Player } from "./Player";
+import { Round } from "./Round";
 import { Table } from "./Table";
 
 export class Game {
@@ -7,12 +8,15 @@ export class Game {
 	private roundNumber: number = 1;
 	/**	The players playing the game */
 	private players: Player[];
+	/**	The round being played */
+	private round: Round;
 	/**	The game's settings */
 	private settings: GameSettings;
 	/**	The table object. Tracks play and whatnot */
 	private table: Table;
 	constructor(input: GameInput) {
 		this.players = input.players;
+		this.round = new Round();
 		this.settings = input.settings;
 		this.table = new Table();
 	}
