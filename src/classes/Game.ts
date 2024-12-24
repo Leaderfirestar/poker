78eb19577951c98d941f6ae2f572b3e50dfe9e61
leaderfirestar <EricAWebb2000@yahoo.com>
2024-12-23 18:44:07 -0600

    comments on round number

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -2,7 +2,8 @@ import { Player } from "./Player";
 import { Table } from "./Table";
 
 export class Game {
-	private currentRound: number = 0;
+	/**	The round number the game is on */
+	private roundNumber: number = 1;
 	/**	The players playing the game */
 	private players: Player[];
 	/**	The table object. Tracks play and whatnot */
