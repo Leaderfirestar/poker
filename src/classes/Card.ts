602e16b02224d76ded5cab74cad6bfea0bda97ac
leaderfirestar <EricAWebb2000@yahoo.com>
2025-01-19 21:28:55 -0600

    cuit plus value

--- src/classes/Card.ts
+++ src/classes/Card.ts
@@ -1,15 +1,23 @@
 type Suit = "Heart" | "Diamond" | "Club" | "Spade";
-type CardValue = "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
+type CardText = "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
+
+interface CardDetail {
+	text: CardText;
+	value: number;
+}
 
 export class Card {
 	/**	Whether or not the card is flipped (for the river) */
 	private flipped: boolean = false;
 	/**	The Card's suit */
 	private suit: Suit;
+	/**	The Card's text */
+	private text: CardText;
 	/**	The Card's value */
-	private value: CardValue;
+	private value: number;
 	constructor(input: CardInput) {
 		this.suit = input.suit;
+		this.text = input.text;
 		this.value = input.value;
 	}
 
@@ -24,8 +32,62 @@ export class Card {
 
 interface CardInput {
 	suit: Suit;
-	value: CardValue;
+	text: CardText;
+	value: number;
 }
 
 export const suits: Suit[] = ["Club", "Diamond", "Heart", "Spade"];
-export const cardValues: CardValue[] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+export const cardDetails: CardDetail[] = [
+	{
+		text: "2",
+		value: 0
+	},
+	{
+		text: "3",
+		value: 1
+	},
+	{
+		text: "4",
+		value: 2
+	},
+	{
+		text: "5",
+		value: 3
+	},
+	{
+		text: "6",
+		value: 4
+	},
+	{
+		text: "7",
+		value: 5
+	},
+	{
+		text: "8",
+		value: 6
+	},
+	{
+		text: "9",
+		value: 7
+	},
+	{
+		text: "10",
+		value: 8
+	},
+	{
+		text: "J",
+		value: 9
+	},
+	{
+		text: "Q",
+		value: 10
+	},
+	{
+		text: "K",
+		value: 11
+	},
+	{
+		text: "A",
+		value: 12
+	}
+];
