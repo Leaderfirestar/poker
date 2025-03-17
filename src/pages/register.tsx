4fe1cbb82ad6758b13fb64f03bc292aed7eb8ce0
leaderfirestar <EricAWebb2000@yahoo.com>
2025-03-16 21:32:01 -0500

    rfce'd

---
+++ src/pages/register.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import styles from "./Register.module.css";
+
+interface Props { }
+
+function Register(props: Props) {
+	return (
+		<div>
+
+		</div>
+	);
+}
+
+export default Register;
